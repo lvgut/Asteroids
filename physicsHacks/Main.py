33646f9bb5931e0ee89cd6cd8077930ed8de35a6
Harsh Patel <hpatel1223@gmail.com>
2018-11-03 13:58:57 -0400

    Updated getData.

--- physicsHacks/Main.py
+++ physicsHacks/Main.py
@@ -1,29 +1,29 @@
-import pygame 
-import sys 
+import pygame
+import sys
 
-class Main: 
-    
+class Main:
+
     # set up window
     size = (1000, 1000)
     screen = pygame.display.set_mode(size)
     BLACK = [0, 0, 0]
     screen.fill(BLACK)
     pygame.display.set_caption("Asteroids")
-    
-    earth_img = pygame.image.load('C:\Users\\bosil\Documents\Personal\PythonProjects3\phys_hacks\images\earth.png').convert_alpha()
-    
-    pygame.init() 
 
-    # main program loop 
-    done = False 
-    while not done: 
+    earth_img = pygame.image.load('C:\Users\\bosil\Documents\Personal\PythonProjects3\phys_hacks\images\earth.png').convert_alpha()
+
+    pygame.init()
+
+    # main program loop
+    done = False
+    while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                done = True 
-                sys.exit() 
-        
-        
+                done = True
+                sys.exit()
+
+
         screen.blit(earth_img, [200, 200])
         pygame.display.flip()
-        
+
 Main()
--- physicsHacks/getData.py
+++ physicsHacks/getData.py
@@ -1,35 +1,39 @@
 import requests
 import json
+import Asteroid
 
-# Parameters for the search, feel free to edit <3
-startDate = "2015-09-07"
-endDate = "2015-09-08"
-apiKey = "jWyOLoAzhBbJ82DBDMFLS47BL5juRll6uUrVeUic"
-parameters = {"start_date": startDate, "end_date": endDate, "api_key": apiKey}
+def createAsteroids:
+    asteroids = [];
 
-# Gets response in a string <3
-response = requests.get("https://api.nasa.gov/neo/rest/v1/feed", params=parameters)
-print(response.status_code)
+    # Parameters for the search, feel free to edit <3
+    startDate = "2015-09-07"
+    endDate = "2015-09-08"
+    apiKey = "jWyOLoAzhBbJ82DBDMFLS47BL5juRll6uUrVeUic"
+    parameters = {"start_date": startDate, "end_date": endDate, "api_key": apiKey}
 
-# Puts response in data object that is of type JSON <3
-data = json.loads(response.content);
+    # Gets response in a string <3
+    response = requests.get("https://api.nasa.gov/neo/rest/v1/feed", params=parameters)
+    print(response.status_code)
 
-# Obtains all the asteroids in a JSON array
-asteroidData = data['near_earth_objects']
+    # Puts response in data object that is of type JSON <3
+    data = json.loads(response.content);
+
+    # Obtains all the asteroids in a JSON array
+    asteroidData = data['near_earth_objects']
 
 
-# Loops through every date, and for every date gets each asteroid and prints data
-asteroidNum = 0;
-for date in asteroidData:
-        print(date);
-        dateArray = asteroidData[date]
-        for asteroid in dateArray:
-            asteroidNum += 1
-            print("Asteroid #" + str(asteroidNum))
-            print("Minimum Asteroid Diameter: " + str(asteroid['estimated_diameter']['kilometers']['estimated_diameter_min']))
-            print("Maximum Asteroid Diameter: " + str(asteroid['estimated_diameter']['kilometers']['estimated_diameter_max']))
-            closeApproachData = asteroid['close_approach_data'][0]
-            relativeVelocity = str(closeApproachData['relative_velocity']['kilometers_per_second'])
-            print("Relative Velocity: " + relativeVelocity)
-            missDistance = closeApproachData['miss_distance']['kilometers']
-            print("Miss Distance: " + missDistance)
+    # Loops through every date, and for every date gets each asteroid and prints data
+    asteroidNum = 0;
+    for date in asteroidData:
+            print(date);
+            dateArray = asteroidData[date]
+            for asteroid in dateArray:
+                asteroidNum += 1
+                minDistance = asteroid['estimated_diameter']['kilometers']['estimated_diameter_min']
+                maxDistance = asteroid['estimated_diameter']['kilometers']['estimated_diameter_max']
+                closeApproachData = asteroid['close_approach_data'][0] #Don't mind this...
+                relativeVelocity = closeApproachData['relative_velocity']['kilometers_per_second']
+                missDistance = closeApproachData['miss_distance']['kilometers']
+                a = Asteroid(minDistance, maxDistance, relativeVelocity, missDistance)
+                asteroids.append(a)
+    return asteroids
